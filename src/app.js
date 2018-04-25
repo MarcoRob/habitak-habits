f0fabfa9279dc475d4fcff4bd34edc2dd8ae4eff
Rodrigo Reyes <rodrigo.rr.reyes@oracle.com>
2018-04-24 22:04:49 -0500

    Cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import Habit from "./models/habitsModel";
 import bodyParser from "body-parser";
 import routes from "./routes/habitsRoutes";
+import cors from "cors";
 
 // Load environment variables from .env file.
 dotenv.config();
@@ -17,6 +18,17 @@ app.set("port", process.env.PORT || 3000);
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoUrl);
+
+const options = {
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+    credentials: true,
+    methods: "GET, HEAD, OPTIONS, PUT, PATCH, POST, DELETE",
+    origin: "*",
+    preflightContinue: false
+};
+
+// use cors middleware
+app.use(cors(options));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const server = app.listen(app.get("port"), () => {
+const server = app.listen(app.get("port"), '10.43.62.112', () => {
     console.log(("  App is running at http://localhost:%d"), app.get("port"));
     console.log("  Press CTRL-C to stop\n");
     console.log
